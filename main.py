ab5734327b30243f77a7564a33a1e7bd7cc7c508
gutodidonato <gutodidonato@gmail.com>
2025-11-12 10:51:18 -0300

    feat: rota /link teste

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ app = FastAPI()
 def read_root():
     #time.sleep(2 * 60)
     return {"Hello World"}
+
+
+@app.get("/link")
+def retorna_link():
+    time.sleep(2 * 60)
+    return {"link": "https://luishtml.com/"}
